fa2887bbabb4499557cc9e94b2ce331341261270
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-04-22 00:16:35 +0200

    Auth controller working. Closes #5

--- ITBGamesJam/GamesJamApi/DTO/LoginDTO.cs
+++ ITBGamesJam/GamesJamApi/DTO/LoginDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace GamesJamApi.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesJamApi.DTO
 {
     public class LoginDTO
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
